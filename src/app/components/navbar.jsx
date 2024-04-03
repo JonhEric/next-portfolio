1279675d7c44fad49359e9a1911807c30fa1b066
Jonh Eric <130633012+JonhEric@users.noreply.github.com>
2024-04-03 21:42:14 +0300

    fixed bug import

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,6 +1,9 @@
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="h-full w-full flex justify-between items-center p-2 sm:p-4 md:p-8 lg:p-12 bg-red-100">
+      <div>1</div>
+      <div>2</div>
+    </div>
   )
 }
 
